2e4597de0b6fbb3559e590921ce8ff159b612d59
EdwardGoomba <ask@edwardvdesign.com>
2018-01-13 22:10:04 -0500

    Read api data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Import modules
 const https = require('https');
 
+// Set username
 const username = 'edwarddanilyuk';
 
 // Function to print message to console
@@ -13,8 +14,17 @@ function printMessage(username, badgeCount, point) {
 // Connect to API Url (https://teamtreehouse.com/username.json)
 const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
   console.log(response.statusCode);
+
   // Read the data
-  // Parse the data
-  // Print the data
+  response.on('data', data => {
+    let body = '';
+    body += data.toString();
+  });
+
+  response.on('end', () => {
+    // Parse the data
+    console.log(body);
+    // Print the data
+  })
 
 });
