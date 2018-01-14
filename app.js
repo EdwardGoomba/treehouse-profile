73758e36a037123376181d05aeaceccc7ed19ab6
EdwardGoomba <ask@edwardvdesign.com>
2018-01-13 21:51:13 -0500

    Write function to print message

---
+++ app.js
@@ -0,0 +1,11 @@
+// Function to print message to console
+function printMessage(username, badgeCount, point) {
+  const message = `${username} has ${badgeCount} total badge(s) and ${point} points in JavaScript`;
+  console.log(message);
+}
+
+
+// Connect to API Url (https://teamtreehouse.com/username.json)
+// Read the data
+// Parse the data
+// Print the data
